7f265abd411b2d7feae13a542b32a50e949ec445
asahi <asahi5286@gmail.com>
2024-06-18 11:22:04 +0900

    練習問題11-2

--- ShootingGame/src/RandomEnemy.java
+++ ShootingGame/src/RandomEnemy.java
@@ -7,4 +7,10 @@ public class RandomEnemy extends Enemy{
 		super.move();
 		vx=Math.random()*4-2;
 	}
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x-5, y, 10, 20);
+		f.fillRect(x+15, y, 10, 20);
+		f.fillRect(x+5, y+20, 10, 10);
+	}
 }
