2c46846a5c5fc806c655b8e90882372608196655
asahi <asahi5286@gmail.com>
2024-06-25 10:31:12 +0900

    練習問題13-1

--- ShootingGame/src/DropEnemy.java
+++ ShootingGame/src/DropEnemy.java
@@ -2,10 +2,11 @@
 public class DropEnemy extends Enemy{
 	public DropEnemy(double x,double y,double vx,double vy) {
 		super(x, y, vx, vy);
+		life=GameWorld.stage;
 	}
 	public void move() {
 		super.move();
-		vy=vy+0.1;
+		vy=vy+0.1*GameWorld.stage;
 	}
 	public void draw(MyFrame f) {
 		f.setColor(0, 128, 0);
